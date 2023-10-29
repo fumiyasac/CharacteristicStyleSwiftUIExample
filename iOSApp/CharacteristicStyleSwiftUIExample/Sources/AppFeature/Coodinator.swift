0563c32f6d01f6b9a08464d27871155f377f965b
fumiyasac <just1factory@gmail.com>
2023-10-29 23:04:25 +0900

    Extensionの追加

---
+++ iOSApp/CharacteristicStyleSwiftUIExample/Sources/AppFeature/Coodinator.swift
@@ -0,0 +1,1 @@
+// TODO: 画面遷移を司るCoodinator処理を記載する
---
+++ iOSApp/CharacteristicStyleSwiftUIExample/Sources/Extension/StringExtension.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+public extension String {
+
+    // MARK: - Function
+
+    // MEMO: 設定されたUIFontの値から幅を取得する（※Tab型切り替えMenu画面等で活用する）
+    func widthOfString(usingFont font: UIFont) -> CGFloat {
+        let fontAttributes = [NSAttributedString.Key.font: font]
+        let size = self.size(withAttributes: fontAttributes)
+        return size.width
+    }
+}
