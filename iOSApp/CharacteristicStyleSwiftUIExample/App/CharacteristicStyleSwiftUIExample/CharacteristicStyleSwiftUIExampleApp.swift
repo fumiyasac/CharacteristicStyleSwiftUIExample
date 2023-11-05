eed35a4e9088db9366f32342deffd033fcce1189
fumiyasac <just1factory@gmail.com>
2023-11-05 21:05:36 +0900

    コメント追記

--- iOSApp/CharacteristicStyleSwiftUIExample/App/CharacteristicStyleSwiftUIExample/CharacteristicStyleSwiftUIExampleApp.swift
+++ iOSApp/CharacteristicStyleSwiftUIExample/App/CharacteristicStyleSwiftUIExample/CharacteristicStyleSwiftUIExampleApp.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 import AppFeature
 
+// ※実装方針: iOS17(正確にはiOS17.0.0)から利用可能になる機能を活用する
+// その1: SwiftData
+// その2: Observation
+// その3: TipKit
+// 👉 基本的にはMVVMの様な形で上手にまとめられる様にしておく
+
 @main
 struct CharacteristicStyleSwiftUIExampleApp: App {
 
