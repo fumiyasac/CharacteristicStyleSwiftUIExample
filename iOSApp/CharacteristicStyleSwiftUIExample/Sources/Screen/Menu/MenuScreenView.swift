b5a5bebd3e37948945b38c6832ee780241796f61
fumiyasac <just1factory@gmail.com>
2023-11-17 22:11:18 +0900

    画面とViewModelの雛形を作成する

---
+++ iOSApp/CharacteristicStyleSwiftUIExample/Sources/Screen/Menu/MenuScreenView.swift
@@ -0,0 +1,27 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by 酒井文也 on 2023/11/17.
+//
+
+import SwiftUI
+
+public struct SwiftUIView: View {
+
+    // MARK: - Initializer
+
+    public init() {}
+
+    // MARK: - Body
+    
+    public var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - Preview
+
+#Preview {
+    SwiftUIView()
+}
---
+++ iOSApp/CharacteristicStyleSwiftUIExample/Sources/Screen/Menu/MenuViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MenuViewModel.swift
+//
+//
+//  Created by 酒井文也 on 2023/11/17.
+//
+
+import Foundation
