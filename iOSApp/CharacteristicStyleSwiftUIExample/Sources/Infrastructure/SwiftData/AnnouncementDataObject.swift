17be183d7176cd802dd2fca51a8e4ce1baa8a8cf
fumiyasac <just1factory@gmail.com>
2023-11-20 22:01:57 +0900

    SwiftDataクラスを新規追加する

---
+++ iOSApp/CharacteristicStyleSwiftUIExample/Sources/Infrastructure/SwiftData/AnnouncementDataObject.swift
@@ -0,0 +1,40 @@
+//
+//  AnnounceDataObject.swift
+//
+//
+//  Created by 酒井文也 on 2023/11/20.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class AnnouncementDataObject {
+
+    // MARK: - Property
+
+    private(set) var id: Int
+    private(set) var title: String
+    private(set) var category: String
+    private(set) var summary: String
+    private(set) var thumbnailUrl: String
+    private(set) var timestamp: Date
+
+    // MARK: - Initializer
+
+    init(
+        id: Int,
+        title: String,
+        category: String,
+        summary: String,
+        thumbnailUrl: String,
+        timestamp: Date = Date()
+    ) {
+        self.id = id
+        self.title = title
+        self.category = category
+        self.summary = summary
+        self.thumbnailUrl = thumbnailUrl
+        self.timestamp = timestamp
+    }
+}
---
+++ iOSApp/CharacteristicStyleSwiftUIExample/Sources/Infrastructure/SwiftData/AnnouncementDataStore.swift
@@ -0,0 +1,62 @@
+//
+//  AnnouncementDataStore.swift
+//
+//
+//  Created by 酒井文也 on 2023/11/20.
+//
+
+import Foundation
+import SwiftData
+
+// 実装参考:
+// SwiftDataをMVVM（SVVS）のArchitecture内で利用する際の参考
+// https://dev.to/jameson/swiftui-with-swiftdata-through-repository-36d1
+// SVVS参考
+// https://github.com/chatwork/svvs-sample
+
+final class AnnouncementDataStore {
+
+    // MARK: - Property
+
+    private let modelContainer: ModelContainer
+    private let modelContext: ModelContext
+
+    // MARK: - Singleton Instance
+
+    @MainActor
+    static let shared = AnnouncementDataStore()
+
+    @MainActor
+    private init() {
+        self.modelContainer = try! ModelContainer(for: AnnouncementDataObject.self)
+        self.modelContext = modelContainer.mainContext
+    }
+
+    // MARK: - Function
+
+    // MEMO: SwiftDataとのやりとりは「Entity構造体⇔専用Modelクラス」の様にする
+
+    // MEMO: 引数で受け取ったデータを1件追加する
+    func insert(_ announcementDataObject: AnnouncementDataObject) {
+        modelContext.insert(announcementDataObject)
+        do {
+            try modelContext.save()
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+    }
+
+    // MEMO: 格納されているデータを全件更新する
+    func fetchAll() -> [AnnouncementDataObject] {
+        do {
+            return try modelContext.fetch(FetchDescriptor<AnnouncementDataObject>())
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+    }
+
+    // MEMO: 引数で受け取ったデータを1件削除する
+    func delete(_ announcementDataObject: AnnouncementDataObject) {
+        modelContext.delete(announcementDataObject)
+    }
+}
