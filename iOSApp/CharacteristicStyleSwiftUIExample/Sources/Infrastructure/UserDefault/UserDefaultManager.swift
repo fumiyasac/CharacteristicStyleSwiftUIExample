b6a5a97fe5529ac66f2018134a44a498f4dc3f20
fumiyasac <just1factory@gmail.com>
2023-11-28 23:55:20 +0900

    試作品を提供する

--- iOSApp/CharacteristicStyleSwiftUIExample/Sources/Infrastructure/UserDefault/UserDefaultManager.swift
+++ iOSApp/CharacteristicStyleSwiftUIExample/Sources/Infrastructure/UserDefault/UserDefaultManager.swift
@@ -1,3 +1,26 @@
 import Foundation
 
-// TODO: UserDefaultの管理機構をPropertyWrapperで提供する
+// 参考: PropertyWrapperで管理しながらもMVVMアーキテクチャで利用しやすい形にする
+// https://www.avanderlee.com/swift/property-wrappers/
+
+@propertyWrapper
+struct UserDefault<Value> {
+    let key: String
+    let defaultValue: Value
+    var container: UserDefaults = .standard
+
+    var wrappedValue: Value {
+        get {
+            return container.object(forKey: key) as? Value ?? defaultValue
+        }
+        set {
+            container.set(newValue, forKey: key)
+        }
+    }
+}
+
+extension UserDefaults {
+
+    @UserDefault(key: "has_seen_app_introduction", defaultValue: false)
+    static var hasSeenAppIntroduction: Bool
+}
