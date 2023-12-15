ccac43b2c4d5be96e4db3e34d3f6a8093734ecde
fumiyasac <just1factory@gmail.com>
2023-12-16 00:00:03 +0900

    新たなレスポンス定義を追加してみる

--- iOSApp/CharacteristicStyleSwiftUIExample/Sources/Infrastructure/APIClient/Response/GalleriesAPIResponse.swift
+++ iOSApp/CharacteristicStyleSwiftUIExample/Sources/Infrastructure/APIClient/Response/GalleriesAPIResponse.swift
@@ -1,9 +1,9 @@
 import Entity
 import Foundation
 
-struct AnnouncementsAPIResponse: Decodable, Equatable {
+struct GalleryAPIResponse: Decodable, Equatable {
 
-    let result: [AnnouncementEntity]
+    let result: [GalleryEntity]
 
     // MARK: - Enum
 
@@ -13,21 +13,21 @@ struct AnnouncementsAPIResponse: Decodable, Equatable {
 
     // MARK: - Initializer
 
-    init(result: [AnnouncementEntity]) {
+    init(result: [GalleryEntity]) {
         self.result = result
     }
 
     // JSONの配列内の要素を取得する → JSONの配列内の要素にある値をDecodeして初期化する
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: Keys.self)
-        self.result = try container.decode([AnnouncementEntity].self, forKey: .result)
+        self.result = try container.decode([GalleryEntity].self, forKey: .result)
     }
 
     // MARK: - Equatable
 
     // MEMO: Equatableプロトコルに適合させるための処理
 
-    static func == (lhs: AnnouncementsAPIResponse, rhs: AnnouncementsAPIResponse) -> Bool {
+    static func == (lhs: GalleryAPIResponse, rhs: GalleryAPIResponse) -> Bool {
         return lhs.result == rhs.result
     }
 }
