8f88cc78b1db0f08b07f23c8a02e74e1abd3953f
fumiyasac <just1factory@gmail.com>
2023-12-24 13:48:48 +0900

    MenuViewObjectのPropertyを生やす対応

--- iOSApp/CharacteristicStyleSwiftUIExample/Sources/ViewObject/MenuViewObject.swift
+++ iOSApp/CharacteristicStyleSwiftUIExample/Sources/ViewObject/MenuViewObject.swift
@@ -2,4 +2,39 @@ import Foundation
 
 struct MenuViewObject {
     
+    // MARK: - Property
+    
+    let id: Int
+    let name: String
+    let catchCopy: String
+    let summary: String
+    let foodCategory: String
+    let price: Int
+    let rate: Double
+    let thumbnailUrl: URL?
+    let publishedAt: String
+
+    // MARK: - Initializer
+
+    init(
+        id: Int,
+        name: String,
+        catchCopy: String,
+        summary: String,
+        foodCategory: String,
+        price: Int,
+        rate: Double,
+        thumbnailUrl: String,
+        publishedAt: String
+    ) {
+        self.id = id
+        self.name = name
+        self.catchCopy = catchCopy
+        self.summary = summary
+        self.foodCategory = foodCategory
+        self.price = price
+        self.rate = rate
+        self.thumbnailUrl = URL(string: thumbnailUrl) ?? nil
+        self.publishedAt = publishedAt
+    }
 }
