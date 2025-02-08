77bbc1569d55d635f2634bc7b4bdb07255d739bb
fumiyasac <just1factory@gmail.com>
2025-02-08 19:27:00 +0900

    [WIP] matchedGeometryEffectの仮組みをする

--- iOSApp/CharacteristicStyleSwiftUIExample/Sources/Screen/Gallery/GalleryScreenView.swift
+++ iOSApp/CharacteristicStyleSwiftUIExample/Sources/Screen/Gallery/GalleryScreenView.swift
@@ -2,12 +2,21 @@ import Extension
 import SwiftUI
 import ViewStateProvider
 import ViewObject
+import Nuke
+import NukeUI
 
 public struct GalleryScreenView: View {
 
     // MARK: - ViewStateProvider
 
     private let viewStateProvider: GalleryViewStateProvider
+
+    @State private var selectedViewObject: GalleryViewObject?
+    @State private var selectedImage: Image?
+
+    @Namespace private var namespace
+
+    let columns = Array(repeating: GridItem(.flexible()), count: 2)
 
     // MARK: - Initializer
 
@@ -17,22 +26,15 @@ public struct GalleryScreenView: View {
 
     // MARK: - Body
 
+    
     public var body: some View {
         NavigationStack {
             Group {
                 if viewStateProvider.requestStatus == .success {
-                    List {
-                        Section {
-                            ForEach(viewStateProvider.galleryViewObjects, id: \.id) { galleryViewObject in
-                                Text(galleryViewObject.title)
-                            }
-                        } header: {
-                            Text("正常処理OK")
-                        }
-                    }
+                    getScrollView()
                 } else {
+                    
                     Text("GalleryScreen")
-
                 }
              }
             .onFirstAppear {
@@ -42,8 +44,58 @@ public struct GalleryScreenView: View {
             .navigationBarTitleDisplayMode(.inline)
         }
     }
-}
 
-#Preview {
-    GalleryScreenView()
+    @ViewBuilder
+    private func getScrollView() -> some View {
+        VStack(alignment: .leading) {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(viewStateProvider.galleryViewObjects, id: \.id) { galleryViewObject in
+                        LazyImage(url: galleryViewObject.thumbnailUrl) { imageState in
+                            if let cachedImage = imageState.image {
+                                VStack (alignment: .leading) {
+                                    cachedImage
+                                        .resizable()
+                                        .scaledToFit()
+                                        .matchedGeometryEffect(id: "gallery-\(galleryViewObject.id)", in: namespace, isSource: selectedImage == nil)
+                                        .onTapGesture {
+                                            withAnimation(.spring(response: 0.36, dampingFraction: 0.48)) {
+                                                selectedImage = cachedImage
+                                                selectedViewObject = galleryViewObject
+                                            }
+                                        }
+                                    Text(galleryViewObject.title)
+                                        .font(.subheadline)
+                                        .bold()
+                                        .foregroundStyle(.primary)
+                                        .padding(.vertical, 4.0)
+                                    Text(galleryViewObject.subtitle)
+                                        .font(.footnote)
+                                        .foregroundStyle(.secondary)
+                                        .padding(.bottom, 4.0)
+                                    Text("[公開日]:" + galleryViewObject.publishedAt)
+                                        .font(.caption)
+                                        .foregroundStyle(.secondary)
+                                        .padding(.bottom, 8.0)
+                                }
+                            } else {
+                                Color(.white)
+                            }
+                        }
+                    }
+                }
+                .padding(12.0)
+            }
+            .opacity(selectedImage == nil ? 1 : 0)
+        }
+        .overlay {
+            if selectedViewObject != nil {
+                GallerySelectedImageView(
+                    selectedImage: $selectedImage,
+                    selectedViewObject: $selectedViewObject,
+                    namespace: namespace
+                )
+            }
+        }
+    }
 }
