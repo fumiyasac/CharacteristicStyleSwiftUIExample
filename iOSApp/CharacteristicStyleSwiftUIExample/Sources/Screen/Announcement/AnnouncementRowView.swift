4ba32b47c6d0d5f2d72e492960f2b61bae057de5
fumiyasac <just1factory@gmail.com>
2025-02-16 19:24:51 +0900

    AnnouncementRowViewのInitializerを設定する

--- iOSApp/CharacteristicStyleSwiftUIExample/Sources/Screen/Announcement/AnnouncementRowView.swift
+++ iOSApp/CharacteristicStyleSwiftUIExample/Sources/Screen/Announcement/AnnouncementRowView.swift
@@ -1,28 +1,38 @@
-//
-//  AnnouncementRowView.swift
-//  CharacteristicStyleSwiftUIExample
-//
-//  Created by 酒井文也 on 2025/02/16.
-//
-
+import NukeUI
 import SwiftUI
 import ViewObject
 
 struct AnnouncementRowView: View {
 
+    // MARK: - Typealias
+
+    typealias TapStockAnnouncementButtonAction = (Bool) -> Void
+
     // MARK: - Property
 
     private var announcementViewObject: AnnouncementViewObject
 
+    private var tapStockAnnouncementButtonAction: AnnouncementRowView.TapStockAnnouncementButtonAction
+
+    // Favoriteボタン（ハート型ボタン要素）の状態を管理するための変数
+    @State private var isStocked: Bool = false
+
     // MARK: - Initializer
 
-    init(announcementViewObject: AnnouncementViewObject) {
+    init(
+        announcementViewObject: AnnouncementViewObject,
+        tapStockAnnouncementButtonAction: @escaping AnnouncementRowView.TapStockAnnouncementButtonAction
+    ) {
         self.announcementViewObject = announcementViewObject
+        self.tapStockAnnouncementButtonAction = tapStockAnnouncementButtonAction
     }
 
     // MARK: - Body
 
     var body: some View {
-        Text("AnnouncementRowView")
+        VStack(alignment: .leading, spacing: 0.0) {
+            Text("AnnouncementRowView")
+        }
+        .padding(8.0)
     }
 }
